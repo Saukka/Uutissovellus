20314f3111f3f8a1a536bd412ec4e8750b4a3b83
Sauli Sorvari <sauli.sorvari@helsinki.fi>
2021-04-28 22:14:41 +0300

    getenv

--- users.py
+++ users.py
@@ -3,6 +3,8 @@ from db import db
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask import session, render_template, request, redirect
 
+app.secret_key = getenv("SECRET_KEY")
+
 def login(username,password):
     sql = "SELECT password, id, usertype FROM users WHERE username=:username"
     result = db.session.execute(sql, {"username":username})
